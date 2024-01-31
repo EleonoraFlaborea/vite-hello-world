8f466310c09ba984482548b2e3f5409a610e5c4b
EleonoraFlaborea <flaboreaeleonora@gmail.com>
2024-01-31 17:45:11 +0100

    add concert img by dynamic string

--- src/components/AppHero.vue
+++ src/components/AppHero.vue
@@ -1,10 +1,22 @@
 <script>
 export default {
-    name: 'MyJumbo'
+    name: 'MyJumbo',
+    data: () => ({
+        image_name: 'lucca_concert.jpeg'
+    }),
+    methods: {
+        createImagePath(image_name) {
+            const url = new URL(`../assets/img/${image_name}`, import.meta.url);
+            return url.href;
+        }
+    }
 }
 </script>
 
 <template>
+    <figure>
+        <img :src="createImagePath(image_name)" alt="lucca concert">
+    </figure>
     <figure>
         <img src="../assets/img/rolling-stones.jpeg" alt="rolling stones">
     </figure>
